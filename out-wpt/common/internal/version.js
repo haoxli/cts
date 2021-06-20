6047822fae0754ceb43a5994f0a66197cc3ee0aa
haoxli <hao.x.li@intel.com>
2021-06-20 19:22:51 +0000

    Deploying to gh-pages from @ haoxli/cts@3d91e32844eea427011849c156520e9a11b22f5e 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'bcc93e24ad10013efd748151a959323b45cb9590';
+export const version = '3d91e32844eea427011849c156520e9a11b22f5e';
